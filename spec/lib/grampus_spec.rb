43e3b335fec57fe972161725c9873af342ce85fc
Benoît d'Oncieu <bdoncieu@gmail.com>
2014-01-19 13:43:46 +0100

    Use Process.getgpid for tests as well...

--- spec/lib/grampus_spec.rb
+++ spec/lib/grampus_spec.rb
@@ -9,9 +9,9 @@ describe Grampus do
 
   it "kills a process running on a port" do
     p = fork { TCPServer.new(21779).accept }
-    expect { Process.getsid(p) }.not_to raise_error
+    expect { Process.getpgid(p) }.not_to raise_error
     Grampus.kill(21779)
-    expect { Process.getsid(p) }.to raise_error(Errno::ESRCH)
+    expect { Process.getpgid(p) }.to raise_error(Errno::ESRCH)
   end
 
   it "returns the list of killed process ids" do
